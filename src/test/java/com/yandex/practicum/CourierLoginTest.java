6cf6b09bb8c811bf6ffe7215bd2ccf28602908cf
daniil.sosnovsky <dsosnovsky@aquivalabs.com>
2022-12-10 23:24:37 -0300

    all tests for CourierLoginTest

--- src/test/java/com/yandex/practicum/CourierLoginTest.java
+++ src/test/java/com/yandex/practicum/CourierLoginTest.java
@@ -34,6 +34,46 @@ public class CourierLoginTest {
             .and()
             .statusCode(200);
     }
+
+    @Test
+    public void courierCantBeLoginWOAllFields(){
+        String json = "{\"password\": \"14243\"}";
+
+        Response response =
+            given()
+            .header("Content-type", "application/json")
+            .and()
+            .body(json)
+            .when()
+            .post("/api/v1/courier/login");
+
+        System.out.println("response = " + response.asString());
+
+        response
+            .then()
+            .statusCode(400);
+    }
+
+    @Test
+    public void courierWrongPassword(){
+        String json = "{\"login\": \"testDan3\", \"password\": \"14$243\"}";
+
+        Response response =
+            given()
+            .header("Content-type", "application/json")
+            .and()
+            .body(json)
+            .when()
+            .post("/api/v1/courier/login");
+
+        System.out.println("response = " + response.asString());
+
+        response
+            .then()
+            .statusCode(404);
+    }
+
+
     /*
      * 
      * Проверь:
@@ -42,6 +82,6 @@ public class CourierLoginTest {
             система вернёт ошибку, если неправильно указать логин или пароль;
             если какого-то поля нет, запрос возвращает ошибку;
             если авторизоваться под несуществующим пользователем, запрос возвращает ошибку;
-            успешный запрос возвращает id.
+            + успешный запрос возвращает id.
      */
 }
